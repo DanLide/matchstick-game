f8dca5f25761cf5b0fac42c6324de3c0d0ae3914
DanLide <danil.lidzer@gmail.com>
2020-07-22 12:14:46 +0000

    optimization: deleted extra code in config page

--- src/components/Config/ConfigPage.js
+++ src/components/Config/ConfigPage.js
@@ -104,40 +104,25 @@ const ConfigPage = () => {
             ))}
           </Stepper>
           <React.Fragment>
-            {activeStep === steps.length ? (
-              <React.Fragment>
-                <Typography variant="h5" gutterBottom>
-                  Thank you for your order.
-                </Typography>
-                <Typography variant="subtitle1">
-                  Your order number is #2001539. We have emailed your order
-                  confirmation, and will send you an update when your order has
-                  shipped.
-                </Typography>
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                {getStepContent(activeStep)}
-                <div className={classes.buttons}>
-                  {activeStep !== 0 && (
-                    <Button onClick={handleBack} className={classes.button}>
-                      Back
-                    </Button>
-                  )}
-                  {activeStep === steps.length - 1 ? (
-                    <StartGame />
-                  ): (
-                    <Button
-                      variant="contained"
-                      onClick={handleNext}
-                      className={classes.button}
-                    >
-                      Next
-                    </Button>
-                  )}
-                </div>
-              </React.Fragment>
-            )}
+            {getStepContent(activeStep)}
+            <div className={classes.buttons}>
+              {activeStep !== 0 && (
+                <Button onClick={handleBack} className={classes.button}>
+                  Back
+                </Button>
+              )}
+              {activeStep === steps.length - 1 ? (
+                <StartGame />
+              ): (
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  className={classes.button}
+                >
+                  Next
+                </Button>
+              )}
+            </div>
           </React.Fragment>
         </Paper>
       </main>
